6e5231e9379a4bcb525790b14063cf42c289c6a9
Jiahao Chen <jiahao@mit.edu>
2014-04-18 22:55:42 -0400

    Add matvec test for FrobeniusMatrix

--- test/frobenius.jl
+++ test/frobenius.jl
@@ -1,11 +1,18 @@
 m=rand(1:10)
 n=rand(1:10)
-Z = FrobeniusMatrix(m, randn(n))
+Z = FrobeniusMatrix(m, randn(n)) #size m+n
+
+#Special properties
 @test_approx_eq full(inv(Z)) inv(full(Z))
 
+#Matvec product
+b = randn(m+n)
+@test_approx_eq Z*b full(Z)*b
+
+#Malmul product
 Y1 = FrobeniusMatrix(m, randn(n)) #Another one of the same column
 @test_approx_eq full(Y1*Z) full(Y1)*full(Z)
 
 n2=rand(1:10)
 Y2 = FrobeniusMatrix(m+n-n2, randn(n2)) #Probably not the same column
-@test_approx_eq full(Y2*Z) full(Y2)*full(Z) #XXX NOT WORKING
+@test_approx_eq full(Y2*Z) full(Y2)*full(Z)
