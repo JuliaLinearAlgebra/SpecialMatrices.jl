e76d92342bce9216cc788be6a0aa49210330071a
Jiahao Chen <jiahao@mit.edu>
2014-06-24 16:02:09 -0400

    Be more careful with generating Strang matrices. Fix #2
    Strang matrix can only be represented as SymTridiagonal for dimension
    n>1. Otherwise, fallback to Diagonal matrix representation for n==1,
    otherwise throw an error.

--- src/strang.jl
+++ src/strang.jl
@@ -4,7 +4,8 @@ immutable Strang{T} <: AbstractArray{T, 2}
 	n :: Int
 end
 Strang(n::Int) = Strang{Float64}(n)
-strang(T, n)=SymTridiagonal(2ones(T, n),-ones(T, n-1))
+strang(T, n)= n >1 ? SymTridiagonal(2ones(T, n),-ones(T, n-1)) :
+              n==1 ? Diagonal(2one(T)) : error("Invalid dimension ", n)
 
 getindex{T}(S::Strang{T}, i, j) = getindex(strang(T, S.n), i, j)
 size(S::Strang, r::Int) = r==1 || r==2 ? S.n : throw(ArgumentError("Invalid dimension $r"))
