f025eded28f080539ce35b50c695a3d65b37748e
Fabrice Pardo <fabrice.pardo@c2n.upsaclay.fr>
2017-02-16 02:01:50 +0100

    Add 2 Hankel constructor tests

--- test/hankel.jl
+++ test/hankel.jl
@@ -1,3 +1,7 @@
-n=rand(1:10)
+n = 9
 H = Hankel(collect(-n:n))
 @test diag(full(H)) == collect(-n:2:n)
+
+@test full(Hankel([1,2,3,4,5])) == [1 2 3;2 3 4; 3 4 5]
+
+@test_throws ArgumentError Hankel([1,2,3,4])
