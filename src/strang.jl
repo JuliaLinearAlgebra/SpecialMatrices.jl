dc6be42ceaf1ecc2e02edd1c94b550dd478ec8d2
ChrisRackauckas <accounts@chrisrackauckas.com>
2017-08-17 09:49:08 -0700

    Fix Strang printing

--- src/strang.jl
+++ src/strang.jl
@@ -6,8 +6,12 @@ end
 Strang(n::Int) = Strang{Float64}(n)
 strang(T, n)= n >1 ? SymTridiagonal(2ones(T, n),-ones(T, n-1)) :
               n==1 ? Diagonal([2one(T)]) : error("Invalid dimension ", n)
-
-getindex{T}(S::Strang{T}, i, j) = getindex(strang(T, S.n), i, j)
+function getindex{T}(S::Strang{T}, i, j)
+	i == j && return 2
+	abs(i - j) == 1 && return -1
+	0
+end
+getindex{T}(S::Strang{T}, I...) = getindex(S,ind2sub(size(S),I)...)
 size(S::Strang, r::Int) = r==1 || r==2 ? S.n : throw(ArgumentError("Invalid dimension $r"))
 size(S::Strang) = S.n, S.n
 full{T}(S::Strang{T}) = full(strang(T, S.n))
