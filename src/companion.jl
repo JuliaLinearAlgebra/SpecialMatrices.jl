82c997a4e6c588a8318e8d83383436a9f4856c01
Jeff Fessler <fessler@umich.edu>
2021-08-30 11:49:18 -0400

    tab to 4space

--- src/companion.jl
+++ src/companion.jl
@@ -1,4 +1,5 @@
 export Companion
+
 """
 [`Companion` matrix](http://en.wikipedia.org/wiki/Companion_matrix)
 
@@ -60,40 +61,40 @@ function Matrix(C::Companion{T}) where T
     M = zeros(T, size(C)...)
     M[:,end]=-C.c
     for i=1:size(C,1)-1
-    	M[i+1, i] = one(T)
-	end
+        M[i+1, i] = one(T)
+    end
     M
 end
 
 #Linear algebra stuff
 function mul!(C::Companion{T}, b::Vector{T}) where T
-	x = b[end]
-	y = -C.c[1]*x
-	b[2:end] = b[1:end-1]-C.c[2:end]*x
-	b[1] = y
+    x = b[end]
+    y = -C.c[1]*x
+    b[2:end] = b[1:end-1]-C.c[2:end]*x
+    b[1] = y
     b
 end
 *(C::Companion{T}, b::Vector{T}) where T = mul!(C, copy(b))
 
 function mul!(A::Matrix{T}, C::Companion{T}) where T
-	v = Array{T}(undef, size(A,1))
-	for i=1:size(A,1)
-		v[i] =dot(vec(A[i,:]),-C.c)
-	end
-	for i=1:size(A,1), j=1:size(A,2)-1
-		A[i,j] = A[i,j+1]
-	end
-	A[:,end] = v
-	A
+    v = Array{T}(undef, size(A,1))
+    for i=1:size(A,1)
+        v[i] =dot(vec(A[i,:]),-C.c)
+    end
+    for i=1:size(A,1), j=1:size(A,2)-1
+        A[i,j] = A[i,j+1]
+    end
+    A[:,end] = v
+    A
 end
 *(A::Matrix{T}, C::Companion{T}) where T = mul!(copy(A), C)
 
 function inv(C::Companion{T}) where T
-	M = zeros(T, size(C)...)
+    M = zeros(T, size(C)...)
     for i=1:size(C,1)-1
-    	M[i, i+1] = one(T)
-	end
-	d = M[end, 1] = -one(T)/C.c[1]
-	M[1:end-1, 1] = d*C.c[2:end]
+        M[i, i+1] = one(T)
+    end
+    d = M[end, 1] = -one(T)/C.c[1]
+    M[1:end-1, 1] = d*C.c[2:end]
     M
 end
