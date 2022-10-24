b618b82027fe78994920cf3e349b043cba0e2e38
Jeff Fessler <fessler@umich.edu>
2022-10-23 22:03:13 -0400

    Only test isposdef for newer Julia versions

--- test/hilbert.jl
+++ test/hilbert.jl
@@ -7,7 +7,6 @@ H = @inferred Hilbert(n)
 
 @test size(H) == (n,n)
 @test ishermitian(H)
-@test isposdef(H)
 @test (@inferred Matrix(H)) isa Matrix
 
 Hi = @inferred inv(H)
@@ -17,8 +16,12 @@ Hi = @inferred inv(H)
 
 @test size(Hi) == (n,n)
 @test ishermitian(Hi)
-@test isposdef(Hi)
 @test (@inferred Matrix(Hi)) isa Matrix
+
+if VERSION >= v"1.6" # This test fails with 1.0 so exclude it there.
+   @test isposdef(H)
+   @test isposdef(Hi)
+end
 
 Hii = @inferred inv(Hi)
 @test Hii == H
