218a3faf5263ce4675d55501b6ea77f8e632a6d2
Jeff Fessler <JeffFessler@users.noreply.github.com>
2022-10-24 10:45:15 -0400

    Tweak Kahan

--- src/kahan.jl
+++ src/kahan.jl
@@ -1,12 +1,13 @@
 #### Kahan matrix
 
 export Kahan
+
 """
 [`Kahan` matrix](http://math.nist.gov/MatrixMarket/data/MMDELI/kahan/kahan.html)
 
 ```julia
 julia> A=Kahan(5,5,1,35)
-5x5 Kahan{Int64,Int64}:
+5×5 Kahan{Int64,Int64}:
  1.0  -0.540302  -0.540302  -0.540302  -0.540302
  0.0   0.841471  -0.454649  -0.454649  -0.454649
  0.0   0.0        0.708073  -0.382574  -0.382574
@@ -14,7 +15,7 @@ julia> A=Kahan(5,5,1,35)
  0.0   0.0        0.0        0.0        0.501368
 
 julia> A=Kahan(5,3,0.5,0)
-5x3 Kahan{Float64,Int64}:
+5×3 Kahan{Float64,Int64}:
  1.0  -0.877583  -0.877583
  0.0   0.479426  -0.420735
  0.0   0.0        0.229849
@@ -22,15 +23,17 @@ julia> A=Kahan(5,3,0.5,0)
  0.0   0.0        0.0
 
 julia> A=Kahan(3,5,0.5,1e-3)
-3x5 Kahan{Float64,Float64}:
+3×5 Kahan{Float64,Float64}:
  1.0  -0.877583  -0.877583  -0.877583  -0.877583
  0.0   0.479426  -0.420735  -0.420735  -0.420735
  0.0   0.0        0.229849  -0.201711  -0.201711
 ```
 
-For more details see: N. Higham, A Survey of Condition Number Estimation
-for Triangular Matrices, SIMAX, Vol. 29, No. 4, pp. 588, 1987,
-http://eprints.ma.man.ac.uk/695/01/covered/MIMS_ep2007_10.pdf
+For more details see:
+N. Higham,
+"A Survey of Condition Number Estimation for Triangular Matrices,"
+SIMAX, Vol. 29, No. 4, pp. 588, 1987,
+https://doi.org/10.1137/1029112
 """
 struct Kahan{T<:Number,T1<:Number} <: AbstractMatrix{T}
     m::Int # dimension
@@ -40,7 +43,7 @@ struct Kahan{T<:Number,T1<:Number} <: AbstractMatrix{T}
 end # immutable
 
 # Define its size
-size(A::Kahan, r::Int) = r==1 ? A.m : A.n
+size(A::Kahan, r::Int) = r==1 ? A.m : r==2 ? A.n : 1
 size(A::Kahan) = A.m, A.n
 
 # Index into a Kahan
@@ -56,5 +59,5 @@ function getindex(A::Kahan,i::Integer,j::Integer)
     end
 end # getindex
 
-# Dense version of Kahan
-Matrix(A::Kahan) =[A[i,j] for i=1:size(A,1), j=1:size(A,2)]
+# Dense version of Kahan (in Core)
+#Matrix(A::Kahan) =[A[i,j] for i=1:size(A,1), j=1:size(A,2)]
