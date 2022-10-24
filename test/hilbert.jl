00e3179497454624948dd348c71a2e6702abf839
Jeff Fessler <fessler@umich.edu>
2022-10-23 21:32:27 -0400

    Test hilbert

--- test/hilbert.jl
+++ test/hilbert.jl
@@ -1,3 +1,34 @@
-H = Hilbert(10)
-@test H[10,10] == 1//(10+10-1)
-@test inv(H) == InverseHilbert(10)
+# test/hilbert.jl
+
+n = 10
+H = @inferred Hilbert(n)
+@test H isa Hilbert
+@test H[n,n] == 1//(2n-1)
+
+@test size(H) == (n,n)
+@test ishermitian(H)
+@test isposdef(H)
+@test (@inferred Matrix(H)) isa Matrix
+
+Hi = @inferred inv(H)
+@test Hi isa InverseHilbert
+@test Hi == InverseHilbert(n)
+@test Hi[1,1] == n^2//1
+
+@test size(Hi) == (n,n)
+@test ishermitian(Hi)
+@test isposdef(Hi)
+@test (@inferred Matrix(Hi)) isa Matrix
+
+Hii = @inferred inv(Hi)
+@test Hii == H
+@test Hi * H == I
+
+# For "large" n the determinant overflows for Int type:
+@test_throws OverflowError det(H)
+# But it works fine for Float64 type:
+Hf = Hilbert(Float64, n)
+@test (@inferred det(Hf)) isa Float64
+
+Hs = Hilbert(5)
+@test (@inferred det(Hs)) ≈ det(Matrix(Hs))
